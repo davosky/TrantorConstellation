5850f663ae5afd499bab7e1a158b8c94bc5c85a4
Davo Davosky <davodavosky@gmail.com>
2024-04-26 17:33:41 +0200

    Locales

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module TrantorConstellation
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # Locales
+    config.i18n.default_locale = :it
+    config.time_zone = "Europe/Rome"
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
