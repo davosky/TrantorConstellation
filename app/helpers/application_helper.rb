9e9141c4dc4f5f79d7e060d32670af488c3c931a
Davo Davosky <davodavosky@gmail.com>
2024-04-27 18:29:01 +0200

    Navbar helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def full_user
+    "#{current_user.first_name} #{current_user.last_name}"
+  end
+
+  def navbar_icon
+    case
+    when current_page?(root_path)
+      "navbar/home-icon".html_safe
+    when current_page?(edit_user_registration_path)
+      "navbar/user-edit-icon".html_safe
+    else
+      "navbar/page-icon".html_safe
+    end
+  end
+
+  def navbar_title
+    case
+    when current_page?(root_path)
+      t "root_path".html_safe
+    when current_page?(edit_user_registration_path)
+      t "user_edit_path".html_safe
+    else
+      t "generic_path".html_safe
+    end
+  end
 end
